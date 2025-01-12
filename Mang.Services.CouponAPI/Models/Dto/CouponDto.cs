8a6ebdc95900969fa18841921f02ba5d6c37e1d2
yousefhasan <yousefhasan@gmail.com>
2025-01-12 10:58:42 +0200

    Coupon Service

--- Mang.Services.CouponAPI/Models/Dto/CouponDto.cs
+++ Mang.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -6,6 +6,6 @@
         public string CouponCode { get; set; }
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
-        //public DateTime LastUpdaet { get; set; }
+        
     }
 }
